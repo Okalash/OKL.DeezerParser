65adadf09fb11adb3b992235ef7c57b65d32aa64
Okalash <43669155+Okalash@users.noreply.github.com>
2019-06-28 02:39:09 +0300

    Init project

--- dsadadsadsa/MainWindow.xaml.cs
+++ dsadadsadsa/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
-﻿using HtmlAgilityPack;
-using Newtonsoft.Json.Linq;
-using System;
+﻿using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace DeezerParser
 {
@@ -36,8 +25,8 @@ namespace DeezerParser
         private async Task GetInfoAsync(string linkToParse)
         {
             var client = new HttpClient();
-            var arrayLink = linkToParse.Split('/');   
-            string html = await client.GetStringAsync("https://api.deezer.com/playlist/"+ arrayLink[arrayLink.Length-1]);
+            var arrayLink = linkToParse.Split('/');
+            string html = await client.GetStringAsync("https://api.deezer.com/playlist/" + arrayLink[arrayLink.Length - 1]);
             var playlist = JObject.Parse(html);
             var songs = new List<Song>();
             foreach (var track in playlist["tracks"]["data"])
@@ -58,12 +47,13 @@ namespace DeezerParser
                 });
 
             }
-            
+
             SongsGrid.ItemsSource = songs;
             SongsGrid.CanUserDeleteRows = false;
             SongsGrid.CanUserAddRows = false;
             SongsGrid.CanUserReorderColumns = false;
             SongsGrid.IsReadOnly = true;
+
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
